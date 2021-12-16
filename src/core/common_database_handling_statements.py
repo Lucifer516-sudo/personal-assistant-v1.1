d3e95c6edc4128aba9724eb97d846b8e1949987e
Lucifer516-sudo <harishvengat02092005@gmail.com>
2021-12-16 20:42:37 +0530

    deleted:    menu/info.txt 	deleted:    prechecks/__pycache__/check_for_main_dir_if_not_exists_create_it.cpython-310.pyc 	deleted:    prechecks/__pycache__/function_check_internet.cpython-310.pyc 	deleted:    prechecks/check_for_main_dir_if_not_exists_create_it.py 	deleted:    prechecks/function_check_internet.py 	deleted:    run_pa.py 	deleted:    signup_.py 	new file:   ../core/common_database_handling_statements.py 	deleted:    ../core/sqlite_execute_stmnt.py

---
+++ src/core/common_database_handling_statements.py
@@ -0,0 +1,28 @@
+import sqlite3
+from typing import Dict, List
+
+class DB:
+    """
+    Got an new approach towards this project and so this class will hold most or
+    all necessary functions for the handling of data ...
+    ___________________________________________________________________________
+    NOTE :
+        This will only have the common data handling functionalities
+    ___________________________________________________________________________
+    """
+    def __init__(self,db_connection,table_name,row_data) -> None:
+        self.db_name = db_name
+        self.table_name = table_name
+        self.row_data = row_data
+
+    def connect_to_database(self):
+        pass
+
+    def write(self,data):
+        """
+        Common statements :
+            table creation => CREATE TABLE IF NOT EXISTS `table name` (`row1` type, `row2` type, ... 'rowx' type)
+            insertion of data => INSERT INTO `table name` VALUES (?, ?, ?, ... ?)
+
+        """
+        database = sqlite3.connect(self)
