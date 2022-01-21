f51267878eec28bb95fa449af28dd3d232c875d2
Lucifer516-sudo <harishvengat02092005@gmail.com>
2022-01-21 18:06:07 +0530

    new file:   __pycache__/global_info.cpython-310.pyc 	modified:   data_handler/render_text.py 	modified:   database_handler/__pycache__/common_database_handling_statements.cpython-310.pyc 	new file:   logger/__pycache__/log_statements.cpython-310.pyc

--- src/core/data_handler/render_text.py
+++ src/core/data_handler/render_text.py
@@ -3,10 +3,36 @@
 """
 
 from src.core.logger.log_statements import Logging # Logging() --> class
+from rich import print as rprint
 
 class Text():
     def __init__(self,text):
         self.text = text
+        __type__ = "Text"
+
+    def split_by_space(self):
+        return str(self.text).split(" ")
+
+    def split_by_comma(self):
+        return str(self.text).split(",")
+#
+#    def pprint(self):
+#        rprint(self.text)
+#        return None
+
+class Censored(Text):
+    def __init__(self,text) -> None:
+        self.text = text
+
+    def censor(self):
+        # Shall only be used while logging
+        if self.__init__.__type__ == "Text" or self.__init__.__type__ == "Password" :
+            pass
+    
+        
+    def uncensor(self):
+        # Raises error on censor()
+        ...
 
 class Password(Text):
     def __init__(self, text):
@@ -19,3 +45,7 @@ class Email(Text):
 class DOB(Text):
     def __init__(self, text):
         super().__init__(text)
+
+c = Censored("test text")
+print((c.censor))
+#print(c.censor())
