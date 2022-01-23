0770518ba11820eff03987a983852917bd6afe75
Lucifer516-sudo <harishvengat02092005@gmail.com>
2022-01-23 17:39:11 +0530

    new file:   src/core/data_handler/hasher.py 	modified:   src/core/database_handler/__pycache__/common_database_handling_statements.cpython-310.pyc

---
+++ src/core/data_handler/hasher.py
@@ -0,0 +1,16 @@
+import hashlib
+from src.core.logger.log_statements import Logging
+from src.core.global_info import file_name
+
+class Hash:
+    def __init__(self,text) -> None:
+        self.text = text
+        self.__length_of_text__ = len(str(self.text))
+        self.logger = Logging()
+        self.log = self.logger.log
+    
+    def hash(self):
+        self.log(log_statement=f"On >> [method] :: Hash.hash [{file_name(__file__)}]")
+        return hashlib.sha512(self.text).hexdigest().encode()
+passwd = b'123'
+print(Hash(passwd).hash())
